8b846ee197a2074ab098cc4ed1727f683c83e802
Patrycia <lim41@uwindsor.ca>
2024-07-26 15:29:43 -0600

    edit: cart.php (block cart access for unauthenticated users, redirect them to index.php)

--- cart.php
+++ cart.php
@@ -1,7 +1,12 @@
 <?php
 require_once('common.php');
-
-$page = new demo(pageid::CART, new cart_page(), array('css/cart.css'), array('js/cart.js'));
-$page->generate();
+// redirect to login.php if the user is not logged in. Access to cart it blocked.
+if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
+    header('Location: index.php');
+    exit();
+} else {
+    $page = new demo(pageid::CART, new cart_page(), array('css/cart.css'), array('js/cart.js'));
+    $page->generate();
+}
 
 ?>
