68102c9ac5eef49876aa1a8aa3dcd9d29ee5c5a3
Witold Kowalski <kowalskw@uwindsor.ca>
2024-07-26 17:23:28 -0400

    updated contact us page added tinymce

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 require_once('common.php');
 
-$page = new demo(pageid::CONTACT, new contact_page(), array('css/mainstyle.css'), array());
+$page = new demo(pageid::CONTACT, new contact_page(), array('css/contact.css'), array());
 $page->generate();
 
 ?>
